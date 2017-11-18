d91413e8ab638471ea07aa306e79b6514276a4a5
fzingg <fredericzingg@protonmail.com>
2017-11-18 19:15:15 +0000

    fre

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -8,15 +8,15 @@ end
 
         
 Hyperloop.configuration do |config|
-  config.transport = :pusher
-  config.channel_prefix = "Hyperloop"
-  config.opts = {
-    app_id: '376259',
-    key: 'bd7704d4f66e46ef896f',
-    secret: '871f3d39cf9b399f0b34',
-    cluster: 'eu',
-    encrypted: true
-  }
+  config.transport = :simple_poller
+  # config.channel_prefix = "Hyperloop"
+  # config.opts = {
+  #   app_id: '376259',
+  #   key: 'bd7704d4f66e46ef896f',
+  #   secret: '871f3d39cf9b399f0b34',
+  #   cluster: 'eu',
+  #   encrypted: true
+  # }
   
 end
 
